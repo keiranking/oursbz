10ffca7d309446db8f64e83dbd719b487b17d9bc
Keiran King <me@keiranking.com>
2025-07-11 18:38:14 -0400

    🔨 #15: Stimulus controller for contact form
    This commit adds a simple controller for the contact form:
    - checks honeypot fields before submitting, to catch some spam
    
    Future commits may provide more robust and granular validation.
    
    Closes #15.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import { Application } from "stimulus";
+import ContactFormController from "./config/controllers/contact_form_controller";
 
 const application = Application.start();
 console.log("Stimulus started.");
+
+application.register("contact-form", ContactFormController);
---
+++ src/config/controllers/contact_form_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = [
+    "sugarcube",
+    // "name",
+    "email"
+    // "org",
+    // "bia",
+    // "biaList",
+    // "message"
+  ];
+
+  isEmailAddress(string) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(string.trim());
+  }
+
+  isSpam = this.sugarcubeTargets.some(sugarcube => sugarcube.value !== '');
+  
+  validateEmail() {
+    this.hasValidEmail = this.isEmailAddress(this.emailTarget.value);
+    this.emailTarget.setAttribute("aria-invalid", String(!this.hasValidEmail));
+  }
+
+  validate(event) {
+    event.preventDefault();
+    let isValid = true;
+
+    if (this.isSpam || !this.hasValidEmail) {
+      isValid = false;
+    }
+
+    if (isValid) {
+      this.element.submit();
+    } else {
+      console.log("Invalid form submission.")
+    }
+  }
+}
